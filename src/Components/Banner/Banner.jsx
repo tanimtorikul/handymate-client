44efac3e05937f8be863308c17848cdffc9408a3
tanimtorikul <torikulswe@gmail.com>
2023-11-05 15:39:33 +0600

    modified banner to carousel banner using react awesome components

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,33 +1,80 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
 const Banner = () => {
   return (
-    <div
-      className="hero min-h-[700px] relative overflow-hidden"
-      style={{
-        backgroundImage: "url(handy1.jpg)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="hero-overlay bg-opacity-50 absolute inset-0 bg-gray-900"></div>
-      <div className="hero-content text-white relative z-10">
-        <div className="max-w-screen-lg mx-auto">
-          <h1 className="text-5xl font-bold mb-4">
-            Treating Customers as <br /> Our Beloved Family
-          </h1>
-          <p className="text-lg mb-6">
-            Providing the utmost care and service to make your experience <br />
-            exceptional. Let us serve you with dedication and love.
-          </p>
-          <div className="flex gap-4">
-            <button className="btn bg-[#ED1D24] text-white border-0 py-2 px-6 rounded-lg hover:bg-[#FF0D10]">
-              Read More
-            </button>
-            <button className="btn bg-black text-white border-0 py-2 px-6 rounded-lg hover:bg-gray-800">
-              Book Now
-            </button>
+    <div className="my-6">
+      <Carousel showThumbs={true}>
+        <div className="relative md:h-[650px]">
+          <img src="handy1.jpg" alt="Slide 1" />
+          <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col justify-center text-white text-2xl">
+            <div className="max-w-screen-lg mx-auto">
+              <h1 className="text-3xl md:text-5xl font-bold mb-4">
+                Treating Customers as <br /> Our Beloved Family
+              </h1>
+              <p className="text-lg mb-6">
+                Providing the utmost care and service to make your experience{" "}
+                <br />
+                exceptional. Let us serve you with dedication and love.
+              </p>
+              <div className="flex gap-4 justify-center">
+                <button className="btn bg-[#ED1D24] text-white border-0 py-2 px-6 rounded-lg hover:bg-[#FF0D10]">
+                  Read More
+                </button>
+                <button className="btn bg-black text-white border-0 py-2 px-6 rounded-lg hover:bg-gray-800">
+                  Book Now
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+        <div className="relative md:h-[650px]">
+          <img src="handy2.jpg" alt="Slide 2" />
+          <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center text-white text-2xl">
+            <div className="max-w-screen-lg mx-auto">
+              <h1 className="text-3xl md:text-5xl font-bold mb-4">
+                Your Trusted Handyman Service
+              </h1>
+              <p className="text-lg mb-6">
+                We're here to fix, repair, and improve your home with care and
+                expertise. Your satisfaction is our top priority. Let us handle
+                the rest!
+              </p>
+              <div className="flex gap-4 justify-center">
+                <button className="btn bg-[#ED1D24] text-white border-0 py-2 px-6 rounded-lg hover:bg-[#FF0D10]">
+                  Learn More
+                </button>
+                <button className="btn bg-black text-white border-0 py-2 px-6 rounded-lg hover:bg-gray-800">
+                  Contact Us
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative md:h-[650px]">
+          <img src="handy3.jpg" alt="Slide 3" />
+          <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center text-white text-2xl">
+            <div className="max-w-screen-lg mx-auto">
+              <h1 className="text-3xl md:text-5xl font-bold mb-4">
+                Quality Repairs for Happy Homes
+              </h1>
+              <p className="text-lg mb-6">
+                We take pride in our craftsmanship and attention to detail. From
+                small fixes to major renovations, we're dedicated to making your
+                home a better place.
+              </p>
+              <div className="flex gap-4 justify-center">
+                <button className="btn bg-[#ED1D24] text-white border-0 py-2 px-6 rounded-lg hover:bg-[#FF0D10]">
+                  Discover More
+                </button>
+                <button className="btn bg-black text-white border-0 py-2 px-6 rounded-lg hover:bg-gray-800">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel>
     </div>
   );
 };
